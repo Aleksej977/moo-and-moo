288eb2028650f09227df68ff71038fd2345422af
Aleksej977 <oleksiistoliar@gmail.com>
2018-01-08 20:22:32 +0200

    Added modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,48 @@
-$('.slick').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
+$(document).ready(function () {
+    $('.slick').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
-        infinite: true,
-        dots: false
-      }
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
+                }
     },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
     },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
     }
-    
+
   ]
+    });
+});
+
+$('#modal').iziModal({
+    theme: "light",
+    overlayColor: 'rgba(0, 0, 0, 0.4)',
+    overlayClose: true,
+    closeOnEscape: true,
+    closeButton: true,
+    bodyOverflow: true
+});
+$(document).on('click', '.open-form', function (event) {
+    event.preventDefault();
+    $('#modal').iziModal('open', this); // Do not forget the "this"
 });
